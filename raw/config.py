12c5122da1651f5b382567936778a4406999ec48
nisepulvedaa <ni.sepulvedaa@gmail.com>
2025-08-10 16:01:42 -0400

    chore(raw): add config.py from deinsoluciones-base-config:config_arq_002_slim.py

---
+++ raw/config.py
@@ -0,0 +1,17 @@
+nombre_evaluador=""
+nombre_proceso="" #nombre-proceso
+version_arquetipo_raw="" #slim|full
+periodicidad="diario"# diario|mensual|esporadico
+##ORIGIN
+buckets_detino=""
+path_destino=""
+nombre_archivo=""
+##RAW
+proyecto="deinsoluciones-serverless"
+dataset_raw_zone=""
+nombre_tabla_raw=""
+campo_fecha_tabla_raw=""
+fecha_ejecucion=""
+ddl_raw=""
+##
+correos_destinatarios=""
