540ae6f210cb8b18810c6109d08730ac50c5fb54
nisepulvedaa <ni.sepulvedaa@gmail.com>
2025-08-10 16:01:42 -0400

    chore(cleansed): add config.py from deinsoluciones-base-config:config_arq_002_cleansed.py

---
+++ cleansed/config.py
@@ -0,0 +1,18 @@
+proyecto="deinsoluciones-serverless"
+nombre_proceso="" #nombre-proceso
+periodicidad="diario"# diario|mensual|esporadico
+##CLEANSED
+dataset_cleansed_zone=""
+nombre_tabla_cleansed=""
+campo_fecha_tabla_cleansed=""
+fecha_ejecucion=""
+###CREATE
+ddl_satelite=""
+ddl_hub=""
+ddl_link=""
+## INSERT
+insert_satelite=""
+insert_hub=""
+insert_link=""
+##CORREO
+correos_destinatarios=""
